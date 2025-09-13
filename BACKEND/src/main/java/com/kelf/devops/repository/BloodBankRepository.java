9a7b3247b9076ecccdc0f45e0100aaf3e904e0a9
Kamesh <2300031070@kluniversity.in>
2025-09-13 11:32:50 +0530

    Updated backend

---
+++ BACKEND/src/main/java/com/kelf/devops/repository/BloodBankRepository.java
@@ -0,0 +1,11 @@
+package com.kelf.devops.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kelf.devops.model.BloodBank;
+
+@Repository
+public interface BloodBankRepository extends JpaRepository<BloodBank, Integer> {
+
+}
